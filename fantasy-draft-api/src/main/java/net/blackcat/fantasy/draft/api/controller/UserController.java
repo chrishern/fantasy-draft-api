2c594560a99036a463c2627e4ac49490fe46b54f
chrishern <chrishern@hotmail.com>
2014-08-10 10:11:43 +0100

    Changed get authenticated user functionality to return a Manager object when the user is logged in.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/UserController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/UserController.java
@@ -3,6 +3,12 @@
  */
 package net.blackcat.fantasy.draft.api.controller;
 
+import net.blackcat.fantasy.draft.integration.controller.ManagerController;
+import net.blackcat.fantasy.draft.integration.exception.FantasyDraftIntegrationException;
+import net.blackcat.fantasy.draft.manager.Manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,16 +27,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fantasydraft/users")
 public class UserController {
 
+	@Autowired
+	@Qualifier(value = "managerIntegrationController")
+	private  ManagerController managerIntegrationController;
+	
 	@RequestMapping(value = "/authenticated/retrieve/", method = RequestMethod.GET)
-	public @ResponseBody UserDetails authenticatedUser() {
+	public @ResponseBody Manager authenticatedUser() throws FantasyDraftIntegrationException {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		
 		if(authentication == null || !(authentication.getPrincipal() instanceof UserDetails)) {
 			return null;
 		}
 
-		System.out.println("USER: " + ((UserDetails)authentication.getPrincipal()).getUsername());
-		return (UserDetails)authentication.getPrincipal();
+		final String emailAddress = ((UserDetails)authentication.getPrincipal()).getUsername();
+		System.out.println("USER: " + emailAddress);
+		return managerIntegrationController.getManager(emailAddress);
 	}
 	
 	@RequestMapping(value = "/authenticate/", method = RequestMethod.POST)
