7d2c0e88b28c53a3466352a884a10c0470d97331
chrishern <chrishern@hotmail.com>
2014-08-10 23:43:25 +0100

    Added @ResponseStatus annotation to take into account the void return status.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/DraftRoundController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/DraftRoundController.java
@@ -8,12 +8,14 @@ import net.blackcat.fantasy.draft.round.TeamBids;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * REST controller class which provides draft round related operations.
@@ -37,6 +39,7 @@ public class DraftRoundController {
 	 */
 	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/bids", method = RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.OK) 
 	public void makeBids(@RequestBody final TeamBids teamBids) throws FantasyDraftIntegrationException {
 		draftRoundIntegrationController.makeBids(teamBids);
 	}
@@ -49,6 +52,7 @@ public class DraftRoundController {
 	 * @throws FantasyDraftIntegrationException
 	 */
 	@RequestMapping(value = "/round/start", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK) 
 	public void startDraftRound(@RequestParam(value = "leagueId", required = true) final int leagueId, 
 			@RequestParam(value = "phaseNumber", required = true) final int phaseNumber) throws FantasyDraftIntegrationException {
 		draftRoundIntegrationController.startAuctionPhase(leagueId, phaseNumber);
