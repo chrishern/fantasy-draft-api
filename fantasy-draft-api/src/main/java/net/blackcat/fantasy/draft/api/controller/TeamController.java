8be8022f7edd7acf064fb409bfbc3ed8c77c7597
chrishern <chrishern@hotmail.com>
2014-08-26 21:31:01 +0100

    Added API method to populate the first week scores.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/TeamController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/TeamController.java
@@ -6,16 +6,19 @@ package net.blackcat.fantasy.draft.api.controller;
 import java.util.List;
 
 import net.blackcat.fantasy.draft.integration.exception.FantasyDraftIntegrationException;
+import net.blackcat.fantasy.draft.integration.upgrade.PopulateFirstWeekScores;
 import net.blackcat.fantasy.draft.player.Player;
 import net.blackcat.fantasy.draft.team.Team;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * REST controller class which provides team related operations.
@@ -31,6 +34,10 @@ public class TeamController {
 	@Qualifier(value = "teamIntegrationController")
 	private  net.blackcat.fantasy.draft.integration.controller.TeamController teamIntegrationController;
 	
+	@Autowired
+	@Qualifier(value = "populateFirstWeekScores")
+	private PopulateFirstWeekScores upgrade;
+	
 	/**
 	 * Get all {@link Player} objects within the game of a given position.
 	 * 
@@ -42,5 +49,16 @@ public class TeamController {
 	@RequestMapping(value = "/{leagueId}", method = RequestMethod.GET)
 	public @ResponseBody List<Team> getPlayers(@PathVariable int leagueId) throws FantasyDraftIntegrationException {
 		return teamIntegrationController.getCompleteTeams(leagueId);
+	}
+	
+	/**
+	 * Function used to manually populate the first week scores in the database.
+	 * 
+	 * @throws FantasyDraftIntegrationException
+	 */
+	@RequestMapping(value = "/upgrade", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK) 
+	public void upgrade() throws FantasyDraftIntegrationException {
+		upgrade.populateAllTeams();
 	}
 }
