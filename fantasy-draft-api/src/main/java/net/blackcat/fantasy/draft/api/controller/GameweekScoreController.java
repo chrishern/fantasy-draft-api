bdd5b3e64cdfb8430627e1074686a2250bfadfc3
chrishern <chrishern@hotmail.com>
2014-08-31 18:01:33 +0100

    Changes for automating gameweek score calculations.

---
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/GameweekScoreController.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package net.blackcat.fantasy.draft.api.controller;
+
+import net.blackcat.fantasy.draft.fpl.integration.controller.CalculateGameweekPointsController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * REST controller class which provides team related operations.
+ * 
+ * @author Chris
+ *
+ */
+@Controller
+@RequestMapping("/fantasydraft/gameweek")
+public class GameweekScoreController {
+
+	@Autowired
+	@Qualifier(value = "calculateGameweekPointsController")
+	private CalculateGameweekPointsController gameweekPointsController;
+	
+	/**
+	 * Calculate the scores for a given gameweek.
+	 * 
+	 * @param gameweek The gameweek to calculate the scores for.
+	 */
+	@RequestMapping(value = "/score/{gameweek}", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void calculateGameweekScores(@PathVariable int gameweek) {
+		gameweekPointsController.calculateGameweekPoints(gameweek);
+	}
+}
