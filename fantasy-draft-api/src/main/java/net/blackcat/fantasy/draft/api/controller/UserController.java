f29e9ad1f33b54809cc09f223288713e30e2e476
chrishern <chrishern@hotmail.com>
2014-09-01 21:15:03 +0100

    Changes for passing out logged in user details.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/UserController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/UserController.java
@@ -3,6 +3,7 @@
  */
 package net.blackcat.fantasy.draft.api.controller;
 
+import net.blackcat.fantasy.draft.LoggedInUser;
 import net.blackcat.fantasy.draft.integration.controller.ManagerController;
 import net.blackcat.fantasy.draft.integration.exception.FantasyDraftIntegrationException;
 import net.blackcat.fantasy.draft.manager.Manager;
@@ -31,9 +32,14 @@ public class UserController {
 	@Qualifier(value = "managerIntegrationController")
 	private  ManagerController managerIntegrationController;
 	
+	/**
+	 * Retrieve authenticated user details.
+	 * 
+	 * @return Username of the logged in user.
+	 */
 	@RequestMapping(value = "/authenticated/retrieve/", method = RequestMethod.GET)
-	public @ResponseBody Manager authenticatedUser() throws FantasyDraftIntegrationException {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	public @ResponseBody String authenticatedUser() {
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		
 		if(authentication == null || !(authentication.getPrincipal() instanceof UserDetails)) {
 			return null;
@@ -41,11 +47,52 @@ public class UserController {
 
 		final String emailAddress = ((UserDetails)authentication.getPrincipal()).getUsername();
 		System.out.println("USER: " + emailAddress);
-		return managerIntegrationController.getManager(emailAddress);
+		return emailAddress;
 	}
 	
+	/**
+	 * Endpoint used for BA authentication.
+	 * @throws FantasyDraftIntegrationException 
+	 */
 	@RequestMapping(value = "/authenticate/", method = RequestMethod.POST)
-	public void authenticate() {
+	public @ResponseBody LoggedInUser authenticate() throws FantasyDraftIntegrationException {
 		// endpoint for the basic authentication request to pass
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails)) {
+			return null;
+		}
+		
+		final String emailAddress = ((UserDetails)authentication.getPrincipal()).getUsername();
+		
+		return managerIntegrationController.getLoggedInUser(emailAddress);
+	}
+	
+	/**
+	 * Get basic user logged in details.
+	 * 
+	 * @return JSON serialisable object containing logged in details.
+	 * @throws FantasyDraftIntegrationException If any error occurs when obtaining the logged in user details.
+	 */
+	@RequestMapping(value = "/authenticate/retrieve/userdetails", method = RequestMethod.GET)
+	public @ResponseBody LoggedInUser getLoggedInUserDetails() throws FantasyDraftIntegrationException {
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final String emailAddress = ((UserDetails)authentication.getPrincipal()).getUsername();
+		
+		return managerIntegrationController.getLoggedInUser(emailAddress);
+	}
+	
+	/**
+	 * Get full manager details.
+	 * 
+	 * @return JSON serialisable object containing manager details.
+	 * @throws FantasyDraftIntegrationException If any error occurs when obtaining the logged in user details.
+	 */
+	@RequestMapping(value = "/authenticate/retrieve/managerdetails", method = RequestMethod.GET)
+	public @ResponseBody Manager getFullManagerDetails() throws FantasyDraftIntegrationException {
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final String emailAddress = ((UserDetails)authentication.getPrincipal()).getUsername();
+		
+		return managerIntegrationController.getManager(emailAddress);
 	}
 }
