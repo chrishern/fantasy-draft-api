950137b1bd640f79032099a790365d8fe1142a9f
Chris Hern <chrishern81@gmail.com>
2015-05-17 20:23:48 +0100

    Changes for calulcating historical gameweek scores.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/GameweekScoreController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/GameweekScoreController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,11 +29,20 @@ public class GameweekScoreController {
 	private CalculateGameweekPointsController gameweekPointsController;
 	
 	/**
-	 * Calculate the scores for a given gameweek.
+	 * Calculate the scores for the current gameweek.
 	 */
 	@RequestMapping(value = "/score", method = RequestMethod.PUT)
 	@ResponseStatus(value = HttpStatus.OK)
 	public void calculateGameweekScores() {
 		gameweekPointsController.calculateGameweekPoints();
+	}
+	
+	/**
+	 * Calculate the scores for a given gameweek.
+	 */
+	@RequestMapping(value = "/score/{gameweek}", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void calculateHistoricalGameweekScores(@PathVariable final int gameweek) {
+		gameweekPointsController.calculateHistoricalGameweekPoints(gameweek);
 	}
 }
