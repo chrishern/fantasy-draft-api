47ef2afaa21231ce55185210b1eaa8b85eaf8bb8
chrishern <chrishern@hotmail.com>
2015-08-31 19:50:54 +0100

    Changes for making transfer bids, accepting bids, rejecting bids.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/TransferWindowRestController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/TransferWindowRestController.java
@@ -6,14 +6,18 @@ package net.blackcat.fantasy.draft.api.controller;
 import net.blackcat.fantasy.draft.api.security.SecurityUtils;
 import net.blackcat.fantasy.draft.integration.exception.FantasyDraftIntegrationException;
 import net.blackcat.fantasy.draft.integration.facade.TransferWindowFacade;
+import net.blackcat.fantasy.draft.integration.facade.dto.OpenTransferWindowTeamTransfersDto;
 import net.blackcat.fantasy.draft.integration.facade.dto.SellPlayerToPotDto;
+import net.blackcat.fantasy.draft.integration.facade.dto.TransferBidDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +51,43 @@ public class TransferWindowRestController {
 
     	transferWindowFacade.sellPlayerToPot(sellPlayerToPotDto);
 
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/bid", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void makeTransferBid(@RequestBody final TransferBidDto transferBidDto) throws FantasyDraftIntegrationException {
+
+    	transferWindowFacade.makeTransferBid(transferBidDto);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/bid/{transferBidId}/accept", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void acceptTransferBid(@PathVariable final int transferBidId) throws FantasyDraftIntegrationException {
+
+    	final String loggedInUser = SecurityUtils.getLoggedInUser();
+    	
+    	transferWindowFacade.acceptTransferBid(loggedInUser, transferBidId);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/bid/{transferBidId}/reject", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void rejectTransferBid(@PathVariable final int transferBidId) throws FantasyDraftIntegrationException {
+
+    	final String loggedInUser = SecurityUtils.getLoggedInUser();
+    	
+    	transferWindowFacade.rejectTransferBid(loggedInUser, transferBidId);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping("/transferDetail")
+    public @ResponseBody OpenTransferWindowTeamTransfersDto getTransferWindowTeamTransferDetail() throws FantasyDraftIntegrationException {
+    	
+    	final String loggedInUser = SecurityUtils.getLoggedInUser();
+    	
+    	return transferWindowFacade.getTeamTransferWindowTransferDetail(loggedInUser);
     }
     
     // @formatter:on
