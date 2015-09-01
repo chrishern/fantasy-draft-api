d8c5caf9d2bf46a33db7300b8e81d11bb6b8d3ab
chrishern <chrishern@hotmail.com>
2015-09-02 00:02:53 +0100

    Changes for transfer window auction.

--- fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/TransferWindowRestController.java
+++ fantasy-draft-api/src/main/java/net/blackcat/fantasy/draft/api/controller/TransferWindowRestController.java
@@ -6,9 +6,12 @@ package net.blackcat.fantasy.draft.api.controller;
 import net.blackcat.fantasy.draft.api.security.SecurityUtils;
 import net.blackcat.fantasy.draft.integration.exception.FantasyDraftIntegrationException;
 import net.blackcat.fantasy.draft.integration.facade.TransferWindowFacade;
+import net.blackcat.fantasy.draft.integration.facade.dto.AuctionBidsDto;
 import net.blackcat.fantasy.draft.integration.facade.dto.OpenTransferWindowTeamTransfersDto;
 import net.blackcat.fantasy.draft.integration.facade.dto.SellPlayerToPotDto;
+import net.blackcat.fantasy.draft.integration.facade.dto.TeamAuctionStatusDto;
 import net.blackcat.fantasy.draft.integration.facade.dto.TransferBidDto;
+import net.blackcat.fantasy.draft.integration.facade.dto.TransferWindowSummaryDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -88,6 +91,54 @@ public class TransferWindowRestController {
     	final String loggedInUser = SecurityUtils.getLoggedInUser();
     	
     	return transferWindowFacade.getTeamTransferWindowTransferDetail(loggedInUser);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/auctionPhase/{leagueId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK) 
+    public void moveOntoAuctionPhase(@PathVariable final int leagueId) throws FantasyDraftIntegrationException {
+    	
+    	transferWindowFacade.moveTransferWindowOntoAuction(leagueId);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/auctionPhase/{leagueId}", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK) 
+    public void openAuctionPhase(@PathVariable final int leagueId) throws FantasyDraftIntegrationException {
+    	
+    	transferWindowFacade.startAuctionPhase(leagueId);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/auctionPhase/{leagueId}/close", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK) 
+    public void closeAuctionPhase(@PathVariable final int leagueId) throws FantasyDraftIntegrationException {
+    	
+    	transferWindowFacade.closeAuctionPhase(leagueId);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping("/auctionPhase/team/status")
+    public @ResponseBody TeamAuctionStatusDto getTeamAuctionStatus() throws FantasyDraftIntegrationException {
+
+        final String loggedInUser = SecurityUtils.getLoggedInUser();
+
+        return transferWindowFacade.getTeamAuctionStatus(loggedInUser);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/auctionPhase/bid", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void makeBids(@RequestBody final AuctionBidsDto bids) throws FantasyDraftIntegrationException {
+
+    	transferWindowFacade.makeBids(bids);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping("/summary/{leagueId}")
+    public @ResponseBody TransferWindowSummaryDto getTransferWindowSummary(@PathVariable final int leagueId) throws FantasyDraftIntegrationException {
+    	
+    	return transferWindowFacade.getTransferWindowSummary(leagueId);
     }
     
     // @formatter:on
